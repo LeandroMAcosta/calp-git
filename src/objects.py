48e700a772e9e8ea550df9df880ef13d8c3549aa
Joaquin de Francesca <jdefrancesca@ARG-60225-N.fibertel.com.ar>
2022-10-15 20:44:45 -0300

    add base code for object implementation

--- src/objects.py
+++ src/objects.py
@@ -1,14 +1,55 @@
 class ShaFile:
-    ...
+
+    def __init__(self, repo, data=None):
+        self.repo = repo
+        self.data = data
+
+        if data is not None:
+            self.deserialize(data)
+
+    def serialize(self):
+        raise NotImplementedError
+
+    def deserialize(self, data):
+        raise NotImplementedError
 
 
 class Blob(ShaFile):
-    ...
+    name = b'blob'
+
+    def serialize(self):
+        return self.blob_data
+
+    def deserialize(self, data):
+        self.blob_data = data
 
 
 class Commit(ShaFile):
-    ...
+    name = b'commit'
+
+    def serialize(self):
+        ...
+
+    def deserialize(self, data):
+        ...
 
 
 class Tree(ShaFile):
-    ...
+    name = b'tree'
+
+    def serialize(self):
+        ...
+
+    def deserialize(self, data):
+        ...
+
+
+OBJECT_CLASSES = [Blob, Commit, Tree]
+
+OBJECT_CHOICES = {}
+for cls in OBJECT_CLASSES:
+    OBJECT_CHOICES[cls.name] = cls
+
+
+def object_class(name):
+    return OBJECT_CHOICES[name]
